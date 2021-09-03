c38892b75635780c2b1883004796f2379124e197
AKIRA2390 <ando--akira@outlook.com>
2021-09-04 00:47:39 +0900

    操縦桿との通信部分を実装した、たぶん動くはず？

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <esp_now.h>
+
+#include "ControlStick.h"
+
+#define Debug
+controlstick::ControlStick Sticks;
+controlstick::BothHandsData_t BothHandsData;
+
+void RecvCB(const uint8_t *mac, const uint8_t *incomingData, int len) {
+  memcpy(&BothHandsData, incomingData, sizeof(BothHandsData));
+#ifdef Debug
+  Serial.println("Data Received!");
+  Serial.println("Right Hand\tRight Hand\tRight Hand\t");
+  Sticks.DumpData(BothHandsData.RightStick);
+  Serial.println("Left Hand\tLeft Hand\tLeft Hand\t");
+  Sticks.DumpData(BothHandsData.LeftStick);
+  Serial.println("\n");
+#endif
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Sticks.ThisReceives(RecvCB);
+  Sticks.SetupConnection();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() {}
